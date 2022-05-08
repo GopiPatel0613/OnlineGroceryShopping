ce4ec17b54edfbc80430c3e89e8c6f58e8473ca8
Gopi Patel <gopipatel@Gopis-MacBook-Air.local>
2022-05-08 10:46:47 -0500

    deleted Navbar component as it is not needed anymore

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import Navbar from './components/Navbar/Navbar';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
