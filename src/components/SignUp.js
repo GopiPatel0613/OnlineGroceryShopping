3d7c8831a3030e18d8097a9bb2dcbd3028fb1fb7
Gopi Patel <gopipatel@Gopis-MacBook-Air.local>
2022-05-10 10:30:29 -0500

    fixed signup inputs

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import React, { useState } from "react";
-
+import "./SignUp.css"
 import axios from "axios";
 
 
@@ -38,35 +38,59 @@ import axios from "axios";
             console.log(error);         })
     }
     return (
-        <form className="row g-3">
+        <form className="signUpForm">
             <h2> Sign up below!!!</h2>
-                        <div className="col-md-6">
-                 <label htmlFor="inputFirstName" className="form-label">First Name</label>
-                <input name="firstName" value={user.firstName} onChange={userChangeHandler} type="text" className="form-control" id="inputFirstName" />
+            <div className="row">
+                <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                    <label htmlFor="inputFirstName" className="form-label">First Name</label>
+                    <input name="firstName" value={user.firstName} onChange={userChangeHandler} type="text" className="form-control" id="inputFirstName" />
+                    </div>
+                <div className="col-md-3"></div>
             </div>
-            <div className="col-md-6">
-                <label htmlFor="inputLastName" className="form-label">Last Name</label>
-                <input name="lastName" value={user.lastName} onChange={userChangeHandler} type="text" className="form-control" id="inputLastName" />
-             </div>
 
-             <div className="col-md-6">
-                 <label htmlFor="inputEmail4" className="form-label">Email</label>
-                 <input name="email" value={user.email} onChange={userChangeHandler} type="email" className="form-control" id="inputEmail4" />
-             </div>
-             <div className="col-md-6">
-                 <label htmlFor="inputPassword4" className="form-label">Password</label>
-                 <input name="password" value={user.password} onChange={userChangeHandler} type="password" className="form-control" id="inputPassword4" />
-             </div>
-             <div className="col-12">
-                 <div className="form-check">
-                     <input className="form-check-input" type="checkbox" id="gridCheck" />
-                     <label className="form-check-label" htmlFor="gridCheck">
-                         Check me out
-                     </label>
-                 </div>
-             </div>
+            <div className="row">
+                <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                    <label htmlFor="inputLastName" className="form-label">Last Name</label>
+                    <input name="lastName" value={user.lastName} onChange={userChangeHandler} type="text" className="form-control" id="inputLastName" />
+                    </div>
+                <div className="col-md-3"></div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                    <label htmlFor="inputEmail4" className="form-label">Email</label>
+                    <input name="email" value={user.email} onChange={userChangeHandler} type="email" className="form-control" id="inputEmail4" />
+                    </div>
+                <div className="col-md-3"></div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                    <label htmlFor="inputPassword4" className="form-label">Password</label>
+                    <input name="password" value={user.password} onChange={userChangeHandler} type="password" className="form-control" id="inputPassword4" />
+                    </div>
+                <div className="col-md-3"></div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="gridCheck" />
+                        <label className="form-check-label" htmlFor="gridCheck">
+                            Check me out
+                        </label>
+                    </div>
+                    </div>
+                <div className="col-md-3"></div>
+            </div>
+             
              <div className="d-grid gap-2 ">
-                 <button onClick={signUpSubmitHandler} className="bg-dark btn btn-outline-success" type="button">Sign up</button>
+                 <button onClick={signUpSubmitHandler} className="btn btn-outline-success" type="button">Sign up</button>
              </div>
          </form>
      )
